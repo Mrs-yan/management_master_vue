efa2cdb4515b328979dbd39cb7362a208d5ed4fb
Mrs-yan <1132395387@qq.com>
2022-05-21 00:07:37 +0800

    工时字段修复

--- manager-master/src/views/main/employeCT/workHours/config/content.js
+++ manager-master/src/views/main/employeCT/workHours/config/content.js
@@ -2,7 +2,7 @@ export const contentConfig = {
   title: '出工列表',
   propList: [
     { prop: 'taskName', label: '任务名称', minWidth: '100' },
-    { prop: "createTime", label: "开始时间", minWidth: "100" },
+    { prop: "createTime", label: "打卡时间", minWidth: "100" },
 
   ],
   showIndexColumn: true,
